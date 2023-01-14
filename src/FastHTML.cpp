b7aebbb72f2bef0cb7022dcb34d804add7b0eae8
ziajek444 <ziajkowski.marcin@gmail.com>
2023-01-15 00:26:50 +0100

    Update algorithm

--- src/FastHTML.cpp
+++ src/FastHTML.cpp
@@ -9,21 +9,27 @@ static std::string RemoveSpaces(std::string str);
 
 HResponse::HResponse(const std::string *body, std::pair<std::string, std::map<std::string, std::string>> filter)
 {
+	// if html code would have whitespaces between open character '<' and tag name i.e. < div ... >
+	// using body->find(openTagOpenIndex) and body->find(openTagCloseIndex) wont work !!
+	// Removing whitespaces (i.e. by RemoveSpaces function) could be too expensive.
+	// Need to figure it out.
 	const size_t additionalSpace = 1024;
-	std::string tag = '<' + RemoveSpaces(filter.first);
-	size_t open = body->find(tag);  // index { |<tag }
-	size_t close = body->find('>', open + tag.size());  // index { <tag...|> }
+	const std::string tag = RemoveSpaces(filter.first);
+	const std::string openTagName = '<' + tag;
+	const std::string closeTagName = "</" + tag;
+	size_t openTagOpenIndex = body->find(openTagName);  // index { |<tag }
+	size_t openTagCloseIndex = body->find('>', openTagOpenIndex + openTagName.size());  // index { <tag...|> }
 
 	
-	while (open != std::string::npos)
+	while (openTagOpenIndex != std::string::npos)
 	{
 		// check if need to find attrs
 		if (filter.second.size() > 0)
 		{
 			//check if there is any attribute
-			std::string statement = body->substr(open, close - open + 1);  // <tag ... ... >
+			std::string statement = body->substr(openTagOpenIndex, openTagCloseIndex - openTagOpenIndex + 1);  // <tag ... ... >
 			statement = RemoveSpaces(statement);  // <tag...>
-			if (statement.size() > tag.size() + 2)  // must be tag + at least '>' but could be '/'. Dosent matter for attr wich req at least "a=0>" which is 4 characters
+			if (statement.size() > openTagName.size() + 2)  // must be tag + at least '>' but could be '/'. Dosent matter for attr wich req at least "a=0>" which is 4 characters
 			{
 				// attribute exist
 				// check if exists all req attrs
@@ -33,13 +39,15 @@ HResponse::HResponse(const std::string *body, std::pair<std::string, std::map<st
 					if (statement.find(kv.first) != std::string::npos) { continue; }
 					else { ok = false; break; }
 				}
-				if (!ok) 
+				if (!ok)
 				{
 					// not this statement I am fooking for, continue
+					openTagOpenIndex = body->find(openTagName, openTagOpenIndex + openTagName.size());
+					continue;
 				}
 				//check if vals are valid
 				std::vector<size_t> pos;
-				std::string attrStatement = statement.substr(tag.size(), statement.size()- tag.size() - 1);  // <tag|attr1="asd"attr2="qwe"attr3="123"|>
+				std::string attrStatement = statement.substr(openTagName.size(), statement.size()- openTagName.size() - 1);  // <tag|attr1="asd"attr2="qwe"attr3="123"|>
 				if (attrStatement.back() == '/')
 				{ // TODO remove this if statement and handle '/' character
 					attrStatement.erase(attrStatement.end() - 1, attrStatement.end());
@@ -54,6 +62,7 @@ HResponse::HResponse(const std::string *body, std::pair<std::string, std::map<st
 					if (attrStatement.substr(valBegin, valEnd).find(kv.second) != std::string::npos)
 					{
 						// ok
+						continue;
 					}
 					else
 					{
@@ -63,8 +72,59 @@ HResponse::HResponse(const std::string *body, std::pair<std::string, std::map<st
 				}
 				if (finalOk)
 				{
-					// add data from this statement to object
+					//step 1
+					size_t closingTagIndex;
+					closingTagIndex = body->find(closeTagName, openTagCloseIndex + 1);
+
+					//TODO missing exception for npos
+					if (closingTagIndex == std::string::npos)
+					{
+						// exception, there must me closing tag "</tag>"
+					}
+					// C
+					if (openTagCloseIndex + 1 == closingTagIndex)
+					{
+						//there is no data
+						occurrence.push_back("#NO DATA");
+						continue;
+					}
+					// B
+					// closingTagIndex has an index
+					// check if redundant openTagName appear between <tag> and </tag>
+					size_t nextRedundantOpenTagIndex = body->substr(openTagCloseIndex + 1, closingTagIndex).find(openTagName) + openTagCloseIndex + 1;
+					bool tmp = false;
+					if (nextRedundantOpenTagIndex != std::string::npos)
+					{ // found redundant openTagName on nextRedundantOpenTagIndex
+						// nextRedundantOpenTagIndex keeps first redundant open address
+						size_t redundantOpens = 1;
+						while (redundantOpens)
+						{
+							// ups open
+							while (true) {
+								nextRedundantOpenTagIndex = body->substr(nextRedundantOpenTagIndex + openTagName.size(), closingTagIndex).find(openTagName);
+								if (nextRedundantOpenTagIndex != std::string::npos)
+								{
+									redundantOpens++;
+									tmp = true;
+								}
+								else
+								{
+									break;
+								}
+							}
+
+							// shifting closing index to the next maching close tag
+							nextRedundantOpenTagIndex = closingTagIndex + closeTagName.size();
+							closingTagIndex = body->find(closeTagName, closingTagIndex + closeTagName.size());
+							redundantOpens--;
+						}
+					}
+					// data are stored between {openTagCloseIndex + 1} and {closingTagIndex}
+					std::string data = body->substr(openTagCloseIndex + 1, closingTagIndex - (openTagCloseIndex + 1));
+					occurrence.push_back(data);
 				}
+				openTagOpenIndex = body->find(openTagName, openTagOpenIndex + openTagName.size());
+				continue;
 			}
 			else
 			{
@@ -79,7 +139,8 @@ HResponse::HResponse(const std::string *body, std::pair<std::string, std::map<st
 			* 
 			*/
 		} // NO ATTRS
-		open = std::string::npos; // For debug purpose, REMOVE IT !! 
+		openTagOpenIndex = body->find(openTagName, openTagOpenIndex+openTagName.size());
+		// set all again
 	} // WHILE
 }
 
@@ -89,7 +150,9 @@ HResponse::~HResponse()
 
 std::string HResponse::GetData()
 {
-	return "Hello There";
+	if (occurrence.size() > 0) return occurrence.back();
+	
+	return "UPS";
 }
 
 static std::string RemoveSpaces(std::string str)
--- src/mian.cpp
+++ src/mian.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	string s = "";
+	string s = "<tag><p id=\"3\">Cukireczek dla Penny<p></p></p></tag>";
 	std::pair<std::string, std::map<std::string, std::string>> filter;
-	filter.first = "a";
+	filter.first = "p";
 	filter.second["id"] = "3";
 	HResponse hr(&s, filter);
 	cout << "Hello CMake. " << hr.GetData() << endl;
